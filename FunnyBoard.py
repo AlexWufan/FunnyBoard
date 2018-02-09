14d02781ff585861dab52188d1f5770af827b297
AlexWufan <mengnanszw@gmail.com>
2018-02-09 02:13:50 -0800

    add new post

--- FunnyBoard.py
+++ FunnyBoard.py
@@ -1,14 +1,13 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
 
 # @app.route('/marker')
 # def maker():
@@ -22,7 +21,43 @@ def hello_world():
 def test1():
     with app.open_resource('data2.json', 'r') as f:
         data = json.load(f)
-    return render_template('1.html', data = data)
+    return render_template('1.html', data=data)
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    with app.open_resource('data2.json', 'r') as f:
+        data = json.load(f)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        message = request.form['message']
+        action_type = request.form['action_type']
+        cordinates = request.form['cordinates']
+        cordinates = [float(cordinates.split(',')[0]), float(cordinates.split(',')[1])]
+        new_feature = {
+                          "type": "Feature",
+                          "geometry": {
+                              "type": "Point",
+                              "coordinates": cordinates
+                          },
+                          "properties": {
+                              "title": title,
+                              "type": action_type,
+                              "message": message
+                          }
+                      }
+
+        data['features'].append(new_feature)
+
+        with open('data2.json', 'w') as f:
+            json.dump(data, f)
+
+    return render_template('1.html', data=data)
+
 
 # @app.route('/data')
 # def data():
--- read.py
+++ read.py
@@ -2,4 +2,27 @@ import json
 
 with open('data2.json', 'r') as f:
     data = json.load(f)
-print(len(data))
+print(len(data['features']))
+
+if True:
+    # title = request.form['title']
+    # message = request.form['message']
+    # action_type = request.form['action_type']
+    # cordinates = request.form['cordinates']
+    new_feature = {
+                      "type": "Feature",
+                      "geometry": {
+                          "type": "Point",
+                          "coordinates": 'test'
+                      },
+                      "properties": {
+                          "title": 'easd',
+                          "type": 'adasdasd',
+                          "message": 'asdadds'
+                      }
+                  }
+
+    data['features'].append(new_feature)
+
+with open('data2.json', 'w') as f:
+        json.dump(data, f)
